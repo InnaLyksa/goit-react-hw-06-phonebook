8f5ad9f47a30c0de2e616aa2d985022054cb9eb7
InnaLyksa <in.lyksa@gmail.com>
2023-03-04 10:21:14 +0200

    Changes to redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,17 @@
-export const App = () => {
-  return <div></div>;
-};
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { ContactList, ContactForm, Filter } from 'components';
+import { Container, SectionHeader, PageHeader } from './App.styled';
+
+export function App() {
+  return (
+    <Container>
+      <PageHeader>Phonebook</PageHeader>
+      <ContactForm />
+      <SectionHeader>Contacts</SectionHeader>
+      <Filter />
+      <ContactList />
+      <ToastContainer />
+    </Container>
+  );
+}
